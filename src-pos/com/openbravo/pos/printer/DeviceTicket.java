9f91ba1f2dd04af947e195522de21bceab076eb5
Andrey Svininykh <svininykh@gmail.com>
2009-09-24 03:46:14 +0000

    Add support VFD-220

--- src-pos/com/openbravo/pos/printer/DeviceTicket.java
+++ src-pos/com/openbravo/pos/printer/DeviceTicket.java
@@ -102,6 +102,8 @@ public class DeviceTicket {
                 m_devicedisplay = new DeviceDisplaySurePOS(pws.getPrinterWritter(sDisplayParam1, sDisplayParam2));
             } else if ("ld200".equals(sDisplayType)) {
                 m_devicedisplay = new DeviceDisplayESCPOS(pws.getPrinterWritter(sDisplayParam1, sDisplayParam2), new UnicodeTranslatorEur());
+            } else if ("cd5220rus".equals(sDisplayType)) {
+                m_devicedisplay = new DeviceDisplayESCPOS(pws.getPrinterWritter(sDisplayParam1, sDisplayParam2), new UnicodeTranslatorRus());
             } else if ("javapos".equals(sDisplayType)) {
                 m_devicedisplay = new DeviceDisplayJavaPOS(sDisplayParam1);
             } else {
